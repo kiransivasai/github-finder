ff0ffa9ca179be928a2d7e8fe8bf975591ead696
kiransivasai <kiransiva2060@gmail.com>
2021-07-07 19:09:35 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
+
+class App extends Component {
+  state = { users: [], loading: false };
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users");
+    console.log(res.data);
+    this.setState({ users: res.data, loading: false });
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
